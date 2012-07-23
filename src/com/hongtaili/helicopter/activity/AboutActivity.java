3801c2d729185f5cd23a2371b73daef1c289838f
Alvin Li <lxlx1991@gmail.com>
2012-07-23 15:53:41 +0800

    add some activities files and some layout files

---
+++ src/com/hongtaili/helicopter/activity/AboutActivity.java
@@ -0,0 +1,7 @@
+package com.hongtaili.helicopter.activity;
+
+import android.app.Activity;
+
+public class AboutActivity extends Activity {
+
+}
---
+++ src/com/hongtaili/helicopter/activity/CheckConfigActivity.java
@@ -0,0 +1,17 @@
+package com.hongtaili.helicopter.activity;
+
+import com.hongtaili.helicopter.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheckConfigActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.check_config);
+	}
+
+}
